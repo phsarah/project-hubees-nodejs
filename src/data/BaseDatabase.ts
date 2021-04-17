c6003025dc5b06a9e30fc7e236058172d0a0df89
SarahHessel <sarah.phessel@gmail.com>
2021-04-17 19:38:06 -0300

    Conexão knex com o banco de dados criada e arquivo para criação da tabela MYSQL

---
+++ src/data/BaseDatabase.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import knex from "knex";
+import { Knex } from "knex";
+
+dotenv.config();
+
+export default class BaseDatabase {
+    protected static connection: Knex = knex({
+        client: "mysql",
+        connection: {
+           host: process.env.DB_HOST,
+           port: Number(process.env.PORT || "3306"),
+           user: process.env.DB_USER,
+           password: process.env.DB_PASSWORD,
+           database: process.env.DB_DATABASE_NAME,
+        },
+    });
+
+    public static async destroyConnection(): Promise<void> {
+        await BaseDatabase.connection.destroy();
+    }
+}
---
+++ src/sqlSetup.ts
@@ -0,0 +1,25 @@
+import BaseDatabase from "./data/BaseDatabase";
+
+export class MySqlSetup extends BaseDatabase{
+    public async createTable(): Promise<void>{
+        try{
+            await BaseDatabase.connection.raw(`
+                CREATE TABLE IF NOT EXISTS hubees_bike
+                (
+                    id VARCHAR(255) PRIMARY KEY NOT NULL,
+                    color VARCHAR(255) NOT NULL,
+                    number_of_gears INT NOT NULL, 
+                    brand VARCHAR(255) NOT NULL,
+                    model VARCHAR(255) NOT NULL,
+                    price DEC(15,2) NOT NULL 
+                )
+            `)
+            console.log("Table successfully created! :D")
+        }
+        catch(e){
+            throw new Error(e.sqlMessage || e.message)
+        }
+    }
+}
+
+new MySqlSetup().createTable()
