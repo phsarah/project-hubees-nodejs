e7708384324761af548911eddebbde5c616a0fd4
SarahHessel <sarah.phessel@gmail.com>
2021-04-19 14:56:09 -0300

    Implementação de algumas validações, como IsNaN.

--- src/business/bikeBusiness.ts
+++ src/business/bikeBusiness.ts
@@ -12,19 +12,19 @@ export class BikeBusiness{
     public async registerBike(input: BikeInputDTO){
 
         if(!input.color){
-            throw new CustomError(417, "It is necessary to inform the 'color' of the bike")
+            throw new CustomError(417, "It is necessary to inform the 'color' of the bike.")
         }
-        if(!input.numberOfGears){
-            throw new CustomError(417, "It is necessary to inform the 'number of gears' of the bike")
+        if(!input.numberOfGears || isNaN(input.numberOfGears)){
+            throw new CustomError(417, "It is necessary to inform the 'number of gears' (in number) of the bike.")
         }
         if(!input.brand){
-            throw new CustomError(417, "It is necessary to inform the 'brand' of the bike")
+            throw new CustomError(417, "It is necessary to inform the 'brand' of the bike.")
         }
         if(!input.model){
-            throw new CustomError(417, "It is necessary to inform the 'model' of the bike")
+            throw new CustomError(417, "It is necessary to inform the 'model' of the bike.")
         }
-        if(!input.price){
-            throw new CustomError(417, "It is necessary to inform the 'price' of the bike")
+        if(!input.price || isNaN(input.price)){
+            throw new CustomError(417, "It is necessary to inform the 'price' (in number and point) of the bike.")
         }
 
         const id = this.idGenerator.generate()
@@ -34,12 +34,15 @@ export class BikeBusiness{
 
     public async editPrice(id: string, price: number){
         
+        if(isNaN(price)){
+            throw new CustomError(417, "The 'price' entry must be of the numeric type.");
+        }
         await this.bikeDatabase.selectById(id)
 
         await this.bikeDatabase.updatePrice(id, price)
     }
 
-    public async getById(id: string ){
+    public async getById(id: string){
     
         const productData = await this.bikeDatabase.selectById(id)
         
@@ -68,6 +71,10 @@ export class BikeBusiness{
 
         if(!minPrice || !maxPrice){
             throw new CustomError(417, "It is necessary to inform the 'minPrice' and 'maxPrice' that you want to filter.");
+            
+        }
+        if(isNaN(minPrice || maxPrice)){
+            throw new CustomError(417, "The entry must be of the number type.");
             
         }
         const listOfBikes = await this.bikeDatabase.selectAll()
