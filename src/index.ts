9f07b1f9b8fe7fe7b3dfa46695503805c3587cfd
SarahHessel <sarah.phessel@gmail.com>
2021-04-20 14:04:35 -0300

    alteração no index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/bike", bikeRouter);
 
 
-const server = app.listen(process.env.PORT || 3003, () => {
+const server = app.listen(3003, () => {
    if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Server is running in http://localhost:${address.port}`);
