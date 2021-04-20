b7c34e1bec2333befa2d108efa0a49791922cb2d
SarahHessel <sarah.phessel@gmail.com>
2021-04-19 21:55:51 -0300

    Alteração no baseDatabase

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -12,7 +12,7 @@ export default class BaseDatabase {
            port: Number(process.env.PORT || "3306"),
            user: process.env.DB_USER,
            password: process.env.DB_PASSWORD,
-           database: process.env.DB_DATABASE_NAME,
+           database: process.env.DB_NAME,
         },
     });
 
