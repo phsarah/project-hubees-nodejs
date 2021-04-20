ad9f200c13589dce92cea63d9a368fe224c1451a
SarahHessel <sarah.phessel@gmail.com>
2021-04-20 14:01:34 -0300

    Alteração no baseDatabase porque faz parte (deploy do heroku ta foda)

--- src/data/BaseDatabase.ts
+++ src/data/BaseDatabase.ts
@@ -9,7 +9,7 @@ export default class BaseDatabase {
         client: "mysql",
         connection: {
            host: process.env.DB_HOST,
-           port: Number(process.env.PORT || "3306"),
+           port: 3306,
            user: process.env.DB_USER,
            password: process.env.DB_PASSWORD,
            database: process.env.DB_NAME,
